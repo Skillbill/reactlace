0a949e0e537346fd3cd2e9b48a8e9c7a551358c6
pzocchi <pierantonio.zocchi@gmail.com>
2026-01-14 10:41:00 +0000

    Deploying to docs from @ Skillbill/reactlace@dcb0a85ae14ddf6d8d6f12c58ff8eee0fa6bc6e1 🚀

---
+++ assets/RLCheckbox.stories-BBdkOCnN.js
@@ -0,0 +1,10 @@
+import{b as o}from"./RLCrud-BCJRpfNT.js";import"./iframe-Bjw0nMSr.js";import"./preload-helper-C1FmrZbK.js";import"./index-DbCjbJ06.js";const l={title:"Components/Checkbox",component:o,tags:["autodocs"],argTypes:{size:{control:"select",options:["small","medium","large",void 0]}},args:{}},e={args:{checked:!0,label:"I agree with the terms and conditions (RLCheckbox)",rules:[{validateFn:t=>t===!0,message:"The value is required"}]}};var r,a,s;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    checked: true,
+    label: 'I agree with the terms and conditions (RLCheckbox)',
+    rules: [{
+      validateFn: (value: unknown) => value === true,
+      message: 'The value is required'
+    }]
+  }
+}`,...(s=(a=e.parameters)==null?void 0:a.docs)==null?void 0:s.source}}};const d=["Primary"];export{e as Primary,d as __namedExportsOrder,l as default};
---
+++ assets/RLSelect.stories-DOF8rbEc.js
@@ -0,0 +1,5 @@
+import{o as r}from"./RLCrud-BCJRpfNT.js";import"./iframe-Bjw0nMSr.js";import"./preload-helper-C1FmrZbK.js";import"./index-DbCjbJ06.js";const n={title:"Components/Select",component:r,tags:["autodocs"],argTypes:{size:{control:"select",options:["small","medium","large",void 0]},placement:{control:"select",options:["top","bottom",void 0]}},args:{label:"Favorite music genres (RLSelect)",placeholder:"Select your favorite music genre...",options:[{value:"dance",text:"Dance"},{value:"rock",text:"Rock"},{value:"pop",text:"Pop"},{value:"indie",text:"Indie"},{value:"classic",text:"Classic"},{value:"country",text:"Country"},{value:"jazz",text:"Jazz"},{value:"others",text:"Others..."}]}},e={args:{multiple:!0}};var t,o,a;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    multiple: true
+  }
+}`,...(a=(o=e.parameters)==null?void 0:o.docs)==null?void 0:a.source}}};const p=["Multiple"];export{e as Multiple,p as __namedExportsOrder,n as default};
